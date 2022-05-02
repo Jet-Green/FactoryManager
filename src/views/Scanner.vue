741293ab7a902f3a6201d071a88c627f1f41002d
Jet-Green <grishadzyin@gmail.com>
2022-05-02 21:56:14 +0400

    add scanner

--- src/views/Scanner.vue
+++ src/views/Scanner.vue
@@ -1,26 +1,48 @@
 <script setup>
+import { onMounted } from "vue";
 // To use Html5QrcodeScanner
 import { Html5QrcodeScanner } from "html5-qrcode";
 // To use Html5Qrcode
 import { Html5Qrcode } from "html5-qrcode";
+onMounted(() => {
+  let cameraId = null;
+  // This method will trigger user permissions
+  Html5Qrcode.getCameras()
+    .then((devices) => {
+      /**
+       * devices would be an array of objects of type:
+       * { id: "id", label: "label" }
+       */
+      if (devices && devices.length) {
+        cameraId = devices[0].id;
+        // .. use this to start scanning.
+        const html5QrCode = new Html5Qrcode(/* element id */ "reader");
 
-function onScanSuccess(decodedText, decodedResult) {
-  // handle the scanned code as you like, for example:
-  console.log(`Code matched = ${decodedText}`, decodedResult);
-}
-
-function onScanFailure(error) {
-  // handle scan failure, usually better to ignore and keep scanning.
-  // for example:
-  console.warn(`Code scan error = ${error}`);
-}
-
-let html5QrcodeScanner = new Html5QrcodeScanner(
-  "reader",
-  { fps: 10, qrbox: { width: 250, height: 250 } },
-  /* verbose= */ false
-);
-html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+        html5QrCode
+          .start(
+            cameraId,
+            {
+              fps: 10, // Optional, frame per seconds for qr code scanning
+              qrbox: { width: 250, height: 250 }, // Optional, if you want bounded box UI
+            },
+            (decodedText, decodedResult) => {
+              console.log(decodedText);
+              console.log(decodedResult);
+              // do something when code is read
+            },
+            (errorMessage) => {
+              // parse error, ignore it.
+            }
+          )
+          .catch((err) => {
+            // Start failed, handle it.
+          });
+      }
+    })
+    .catch((err) => {
+      // handle err
+    });
+});
 </script>
 <template>
   <div>
