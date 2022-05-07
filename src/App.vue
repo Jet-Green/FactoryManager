5376663c53dc17b756f8292ae9e2b926ae2e6509
Saveliy <62023376+SaveliyShutov@users.noreply.github.com>
2022-05-07 19:32:33 +0400

    update main page and header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script setup>
 </script>
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+<v-app>
+  <v-navigation-drawer v-model="drawer">
+  </v-navigation-drawer>
+
+  <v-app-bar elevation="1" dense>
+  <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <v-app-bar-title>Zavod</v-app-bar-title>
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <!-- <v-container fluid> -->
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    <!-- </v-container> -->
+  </v-main>
+
+  <v-footer>
+    <!-- -->
+  </v-footer>
+</v-app>
 </template>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,9 +2,43 @@
 </script>
 
 <template>
-  <div>
-    <h1>Home</h1>
-    <h1><router-link to="/scanner">QR</router-link></h1>
-  </div>
+  <v-row no-gutters class="text-center font-weight-bold">
+    <v-col cols="12" class="mb-1">
+      <v-hover v-slot="{ isHovering, props }">
+        <v-card :elevation="isHovering ? 3 : 1" v-bind="props">
+          <v-card-content>УХП</v-card-content>
+        </v-card>
+      </v-hover>
+    </v-col>
+    <v-col cols="12" class="mb-1">
+      <v-hover v-slot="{ isHovering, props }">
+        <v-card :elevation="isHovering ? 3 : 1" v-bind="props">
+          <v-card-content>Планирование</v-card-content>
+        </v-card>
+      </v-hover>
+    </v-col>
+    <v-col cols="12" class="mb-1">
+     <v-hover v-slot="{ isHovering, props }">
+        <v-card :elevation="isHovering ? 3 : 1" v-bind="props">
+          <v-card-content>Статус производства</v-card-content>
+        </v-card>
+      </v-hover>
+    </v-col>
+    <v-col cols="12" class="mb-1">
+     <v-hover v-slot="{ isHovering, props }">
+        <v-card :elevation="isHovering ? 3 : 1" v-bind="props">
+          <v-card-content>Отчеты</v-card-content>
+        </v-card>
+      </v-hover>
+    </v-col>
+    <v-col cols="12" class="mb-1">
+     <v-hover v-slot="{ isHovering, props }">
+        <v-card :elevation="isHovering ? 3 : 1" v-bind="props">
+          <v-card-content>...</v-card-content>
+        </v-card>
+      </v-hover>
+    </v-col>
+
+  </v-row>
 </template>
 
