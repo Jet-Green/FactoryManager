0fed2ebbe63fbe57f204d6104e294db8eae3586f
Jet-Green <grishadzyin@gmail.com>
2022-05-12 17:20:41 +0400

    add binding to click

--- src/views/PartyMovement.vue
+++ src/views/PartyMovement.vue
@@ -1,6 +1,66 @@
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+
+const desserts = reactive([
+  {
+    name: "Frozen Yogurt",
+    calories: 159,
+  },
+  {
+    name: "Ice cream sandwich",
+    calories: 237,
+  },
+  {
+    name: "Eclair",
+    calories: 262,
+  },
+  {
+    name: "Cupcake",
+    calories: 305,
+  },
+  {
+    name: "Gingerbread",
+    calories: 356,
+  },
+  {
+    name: "Jelly bean",
+    calories: 375,
+  },
+  {
+    name: "Lollipop",
+    calories: 392,
+  },
+  {
+    name: "Honeycomb",
+    calories: 408,
+  },
+  {
+    name: "Donut",
+    calories: 452,
+  },
+  {
+    name: "KitKat",
+    calories: 518,
+  },
+]);
+
+function goTo(item) {
+  console.log(item);
+}
+</script>
 <template>
-  <div>
-    <h1>movement</h1>
-  </div>
+  <v-table density="compact">
+    <thead>
+      <tr>
+        <th class="text-left">Name</th>
+        <th class="text-left">Calories</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in desserts" :key="item.name" @click="goTo(item)">
+        <td>{{ item.name }}</td>
+        <td>{{ item.calories }}</td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
