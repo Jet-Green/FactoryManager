d1125624232f4fbbb29b189392f4ff007d289368
Jet-Green <grishadzyin@gmail.com>
2022-05-15 15:35:16 +0400

    add template to main page

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,9 +2,25 @@
 </script>
 
 <template>
-  <div>
-    <h1>Home</h1>
-    <h1><router-link to="/scanner">QR</router-link></h1>
-  </div>
+  <v-row>
+    <v-col>Неисправное оборудование</v-col>
+    <v-col>Остановленные партии</v-col>
+  </v-row>
+  <v-row>
+    <v-col>Выполнение СЗ</v-col>
+    <v-col>Выполнение РЦ</v-col>
+    <v-col>Выработка</v-col>
+  </v-row>
+  <v-row>
+    <v-col>Выполнение плана</v-col>
+    <v-col>Точно в срок</v-col>
+  </v-row>
+  <v-row>
+    <v-col>Движение партий</v-col>
+    <v-col>
+      <h1><router-link to="/scanner">QR</router-link></h1>
+    </v-col>
+  </v-row>
 </template>
-
+<style scoped>
+</style>
