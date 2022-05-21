6216542a24bf67f8890c37222e1e0b221f090b96
Saveliy <62023376+SaveliyShutov@users.noreply.github.com>
2022-05-21 20:23:10 +0400

    add icons and fix cardds

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,9 @@ onMounted(() => {
 });
 </script>
 <template>
-<v-app>
-
-
-  <!-- Sizes your content based upon application components -->
-  <v-main>
-
-    <!-- Provides the application the proper gutter -->
-    <!-- <v-container fluid> -->
-
-      <!-- If using vue-router -->
-      <router-view></router-view>
-    <!-- </v-container> -->
-  </v-main>
-
-</v-app>
+  <v-app>
+    <v-main>
+        <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,70 +2,68 @@
 </script>
 
 <template>
-  <v-container class="text-center h-100 w-100">
-    <v-row class="bordored h-25 d-flex justify-center mt-2 align-center">
-      
-      <v-col cols="4">
-        <v-card >
-          <v-card-content>Неисправное оборудование</v-card-content>
-        </v-card>
-      </v-col>
-      <v-col cols="4">
-        <v-card>
-          <v-card-content>Остановленные партии</v-card-content>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row class=" bordored d-flex justify-center align-center h-25">
-      <v-col cols="4">
-        <v-card>
-          <v-card-content>Выполнение СЗ</v-card-content>
-        </v-card>
-      </v-col>
-      <v-col cols="4">
-        <v-card>
-          <v-card-content>Выполнение РЦ</v-card-content>
-        </v-card>
-      </v-col>
-      <v-col cols="4">
-        <v-card>
-          <v-card-content>Выработка</v-card-content>
-        </v-card>
-      </v-col>
-      <v-col cols="4">
-        <v-card>
-          <v-card-content>Выполнение плана</v-card-content>
-        </v-card>
-      </v-col>
-      <v-col cols="4">
-        <v-card>
-          <v-card-content>Точно в срок</v-card-content>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row class="h-50 d-flex justify-center align-end">
-      <v-col>
+  <v-container class="text-center h-100 w-100" no-gutters>
+    <div class="h-50">
+      <v-row class="bordored d-flex justify-center mt-2 align-center">
+        <v-col cols="6">
+          <v-card class="mx-auto">
+            <v-img
+              src="../Icons/settings.png"
+            ></v-img>
+          </v-card>
+        </v-col>
+        <v-col cols="6">
+          <v-card>
+            <v-card-content>Остановленные партии</v-card-content>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row class="bordored d-flex justify-center align-center">
+        <v-col cols="4">
+          <v-card>
+            <v-card-content>Выполнение СЗ</v-card-content>
+          </v-card>
+        </v-col>
+        <v-col cols="4">
+          <v-card>
+            <v-card-content>Выполнение РЦ</v-card-content>
+          </v-card>
+        </v-col>
+        <v-col cols="4">
+          <v-card>
+            <v-card-content>Выработка</v-card-content>
+          </v-card>
+        </v-col>
+        <v-col cols="4">
+          <v-card>
+            <v-card-content>Выполнение плана</v-card-content>
+          </v-card>
+        </v-col>
+        <v-col cols="4">
+          <v-card>
+            <v-card-content>Точно в срок</v-card-content>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+    <v-row class="d-flex justify-center align-end h-50" dense>
+      <v-col cols="6">
         <v-card>
           <v-card-content>Движение партий</v-card-content>
         </v-card>
       </v-col>
-      <v-col>
+      <v-col cols="6">
         <v-card
-          ><v-card-content><h4><router-link to="/scanner">QR</router-link></h4></v-card-content></v-card
+          ><v-card-content
+            ><h4>
+              <router-link to="/scanner">QR</router-link>
+            </h4></v-card-content
+          ></v-card
         >
       </v-col>
     </v-row>
   </v-container>
 </template>
 <style  lang="scss" scoped>
-.v-card-content{
-  font-size: 18px;
-
-  @media (max-width: 768px) {
-    font-size:10px;
-  }
-}
-// .bordored{
-//   border: 4px solid #2e0ffa;
-// }
 </style>
