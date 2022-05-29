bb0716704f4077b385d3478b5c8dd5d27095107e
Saveliy <62023376+SaveliyShutov@users.noreply.github.com>
2022-05-29 13:07:30 +0400

    final fix main page

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -4,11 +4,11 @@
 <template>
   <v-container class="text-center h-100 w-100" no-gutters>
     <div class="h-75">
-      <v-row class="bordored d-flex justify-center mt-2 align-center mb-5 mb-md-8 ">
+      <v-row class="bordored d-flex justify-center mt-2 align-center mb-5 mb-md-8">
         <v-col cols="4">
           <v-card class="pa-4">
             <v-badge color="red" content="4"
-              ><img src="../assets/Icons/settings.png" alt="photo" height="50"
+              ><img src="../assets/Icons/settings.png" alt="photo"
             /></v-badge>
           </v-card>
         </v-col>
@@ -18,7 +18,6 @@
               ><img
                 src="../assets/Icons/search-alt.png"
                 alt="photo"
-                height="50"
             /></v-badge>
           </v-card>
         </v-col>
@@ -31,7 +30,6 @@
               ><img
                 src="../assets/Icons/list-check.png"
                 alt="photo"
-                height="50"
             /></v-badge>
           </v-card>
         </v-col>
@@ -41,7 +39,6 @@
               ><img
                 src="../assets/Icons/chart-histogram.png"
                 alt="photo"
-                height="50"
             /></v-badge>
           </v-card>
         </v-col>
@@ -51,14 +48,13 @@
               ><img
                 src="../assets/Icons/time-check.png"
                 alt="photo"
-                height="50"
             /></v-badge>
           </v-card>
         </v-col>
         <v-col cols="4">
           <v-card class="pa-4">
             <v-badge color="green" content="85%">
-              <img src="../assets/Icons/time-fast.png" alt="photo" height="50"
+              <img src="../assets/Icons/time-fast.png" alt="photo"
             /></v-badge>
           </v-card>
         </v-col>
@@ -68,22 +64,21 @@
               <img
                 src="../assets/Icons/calendar-check.png"
                 alt="photo"
-                height="50"
             /></v-badge>
           </v-card>
         </v-col>
       </v-row>
     </div>
-    <v-row class="d-flex justify-center h-50">
+    <v-row class="d-flex justify-center">
       <v-col cols="6">
         <v-card class="pa-4">
-          <img src="../assets/Icons/map-marker.png" alt="photo" height="50" />
+          <img src="../assets/Icons/map-marker.png" alt="photo"/>
         </v-card>
       </v-col>
       <v-col cols="6">
         <v-card class="pa-4">
           <router-link to="/scanner"
-            ><img src="../assets/Icons/transform.png" alt="photo" height="50"
+            ><img src="../assets/Icons/transform.png" alt="photo"
           /></router-link>
         </v-card>
       </v-col>
@@ -95,6 +90,12 @@
   border: 4px solid #3369a3;
     @media (max-width: 768px) {
     border: 3.5px solid #3369a3;
+  }
+}
+img{
+  height: 50px;
+   @media (max-width: 768px) {
+   height: 30px;
   }
 }
 </style>
