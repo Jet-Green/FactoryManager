ccc6aedef5c2c1956bbe4fdd0e9bc3545f8b6744
Jet-Green <grishadzyin@gmail.com>
2022-06-17 10:14:15 +0400

    add Roboto font

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
-import { loadFonts } from './plugins/webfontloader'
 import { createPinia } from 'pinia'
+import './assets/style/main.scss'
 
-loadFonts()
 
 createApp(App).use(createPinia())
   .use(vuetify)
